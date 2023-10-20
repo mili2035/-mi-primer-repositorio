922b56f889d28b4af10d8624f0e64494787059d3
Milady <Milady@DESKTOP-55DRCMR>
2023-10-19 19:11:51 -0500

    se esta usando el git commit y si esta funcionando,segun...

---
+++ miprimerRepositorio/src/main/java/com/mycompany/miprimerrepositorio/MiprimerRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miprimerrepositorio;
+
+/**
+ *
+ * @author Milady
+ */
+public class MiprimerRepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
